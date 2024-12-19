446ddbafee5d24c61c15ea6445aff6d69d651c64
prabhattopi <topi9864@gmail.com>
2024-12-20 03:16:32 +0530

    the file is save search card dialogdetails

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -5,17 +5,24 @@ import { Search } from "lucide-react";
 interface SearchInputProps {
   value: string;
   onChange: (value: string) => void;
+  onEnter?: () => void; // Optional callback for Enter key
+  
 }
-export const SearchInput = ({ value, onChange }: SearchInputProps) => {
+export const SearchInput = ({ value, onChange,onEnter }: SearchInputProps) => {
     return (
-      <div className="relative max-w-xl mx-auto mt-12">
-        <Search className="absolute left-4 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
+      <div className="relative max-w-2xl mx-auto mt-4 px-4">
+        <Search className="absolute left-8 top-1/2 transform -translate-y-1/2 text-gray-400 w-4 h-4" />
         <Input
           type="text"
-          placeholder="Anjali"
+          placeholder="Search"
           value={value}
+          onKeyDown={(e) => {
+            if (e.key === "Enter" && onEnter) {
+              onEnter(); // Call the onEnter callback if provided
+            }
+          }}
           onChange={(e) => onChange(e.target.value)}
-          className="p-6 pl-10  w-full border-gray-200"
+          className="p-6 pl-10 text-[18px] w-full border-gray-200"
         />
        
       </div>
---
+++ src/components/UserGrid.tsx
@@ -0,0 +1,33 @@
+import { User } from "@/constant";
+import { UserCard } from "./UserCard";
+import NotFound from "@/assets/Not_Found.png";
+interface UserGridProps {
+  users: any;
+  query: string;
+  onFetchDetails: (userId: number) => void;
+  searching:boolean;
+}
+
+export const UserGrid = ({ users, onFetchDetails, searching,query }: UserGridProps) => {
+  console.log(users);
+  return (
+    <>
+      {users?.length == 0 && !searching && query&& (
+        <div className="flex w-full mt-6 items-center justify-center">
+          <img src={NotFound} className="w-64 h-64" />
+        </div>
+      )}
+      {users?.length > 0 && (
+        <div className="grid grid-cols-1 md:grid-cols-2  gap-4 max-w-2xl mx-auto px-4 mt-4">
+          {users?.map((user: User) => (
+            <UserCard
+              key={user.id}
+              user={user}
+              onFetchDetails={onFetchDetails}
+            />
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
